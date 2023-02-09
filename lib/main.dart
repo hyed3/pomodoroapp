320cc31f807ad312edbf672349a56c209329e4cc
hyeji <shonlucy@naver.com>
2023-02-09 16:30:15 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
